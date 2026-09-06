f0b4dffa4bca1f49e2fa230e9bb329db1486284c
devam_mavani <145127104+devam-mavani@users.noreply.github.com>
2026-09-06 13:32:41 +0530

    Bootreceiver-fix

--- app/src/main/java/com/devam/reminderapp/service/BootReceiver.kt
+++ app/src/main/java/com/devam/reminderapp/service/BootReceiver.kt
@@ -3,7 +3,6 @@ package com.devam.reminderapp.service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import androidx.lifecycle.ProcessLifecycleOwner
 import com.devam.reminderapp.data.ReminderDatabase
 import com.devam.reminderapp.util.AlarmScheduler
 import kotlinx.coroutines.CoroutineScope
